0eddb35735e03688ddcd8418f0ac83003bb47c7c
Ilya <hairais@mail.ru>
2020-08-21 16:37:10 +0700

    UPDATE v1.4
    + Правки
    + Начато создание кнопки редактирования

--- PhoneBook_GUI.py
+++ PhoneBook_GUI.py
@@ -2,10 +2,13 @@ from tkinter import *
 from tkinter import ttk
 import os
 
-version = "v1.3"
+version = "v1.4"
 
 phone_book = dict()
 tel_list = list()
+
+BUTTON_WIDTH = 15
+BUTTON_PADX = 30
 
 
 def clear():
@@ -29,6 +32,10 @@ def add():
         phone_book[_tel_] = _value_
 
         list_tel.insert(END, _tel_)
+
+
+def edit():
+    pass
 
 
 def save_in_file():
@@ -72,67 +79,77 @@ def search():
             for val in item:
                 if val == search_text:
                     output_data(key)
+                    label_info.config(text="Программа готова к работе...")
             else:
                 label_info.config(text="Ничего не найдено", fg="red")
 
 
 window = Tk()
 window.title(f"PhoneBook {version}")
-window.geometry("550x270")
+window.geometry("580x290")
 
 label_search = Label(text="Поиск")
 label_search.grid(row=0, column=0, padx=10, pady=5, sticky="e")
 
-input_search = Entry()
-input_search.grid(row=0, column=1, padx=10, pady=5)
+input_search = ttk.Entry()
+input_search.config(width=24)
+input_search.grid(row=0, column=1, padx=10, pady=15)
 
 label_tel = Label(text="Номер телефона")
 label_tel.grid(row=1, column=0, padx=10, pady=5, sticky="w")
 
 input_tel = ttk.Entry()
-input_tel.grid(row=1, column=1)
+input_tel.grid(row=1, column=1, sticky="w")
 
 label_last_name = Label(text="Фамилия")
 label_last_name.grid(row=2, column=0, padx=10, pady=5, sticky="w")
 
 input_last_name = ttk.Entry()
-input_last_name.grid(row=2, column=1)
+input_last_name.grid(row=2, column=1, sticky="w")
 
 label_first_name = Label(text="Имя")
 label_first_name.grid(row=3, column=0, padx=10, pady=5, sticky="w")
 
 input_first_name = ttk.Entry()
-input_first_name.grid(row=3, column=1)
+input_first_name.grid(row=3, column=1, sticky="w")
 
 label_patronymic = Label(text="Отчество")
 label_patronymic.grid(row=4, column=0, padx=10, pady=5, sticky="w")
 
 input_patronymic = ttk.Entry()
-input_patronymic.grid(row=4, column=1)
+input_patronymic.grid(row=4, column=1, sticky="w")
 
 label_address = Label(text="Адрес")
 label_address.grid(row=5, column=0, padx=10, pady=5, sticky="w")
 
 input_address = ttk.Entry()
-input_address.grid(row=5, column=1, pady=10)
+input_address.grid(row=5, column=1, pady=10, sticky="w")
 
 label_info = Label(text="Программа готова к работе...")
 label_info.grid(row=6, column=0, columnspan=26)
 
 button_add = ttk.Button(text="Добавить", command=add)
-button_add.grid(row=1, column=2, padx=38)
+button_add.config(width=BUTTON_WIDTH)
+button_add.grid(row=1, column=2, padx=BUTTON_PADX)
 
 button_save = ttk.Button(text="Сохранить", command=save_in_file)
-button_save.grid(row=2, column=2, padx=38)
+button_save.config(width=BUTTON_WIDTH)
+button_save.grid(row=2, column=2, padx=BUTTON_PADX)
+
+button_edit = ttk.Button(text="Редактировать", command=edit)
+button_edit.config(width=BUTTON_WIDTH)
+button_edit.grid(row=3, column=2, padx=BUTTON_PADX)
 
 button_clear = ttk.Button(text="Удалить", command=clear)
-button_clear.grid(row=3, column=2, padx=38)
+button_clear.config(width=BUTTON_WIDTH)
+button_clear.grid(row=4, column=2, padx=BUTTON_PADX)
 
 label_list_tel = Label(text="Список телефонов")
 label_list_tel.grid(row=0, column=3)
 
 button_search = ttk.Button(text="Найти", command=search)
-button_search.grid(row=0, column=2, padx=10)
+button_search.config(width=BUTTON_WIDTH)
+button_search.grid(row=0, column=2, padx=BUTTON_PADX)
 
 list_tel = Listbox()
 list_tel.grid(row=1, column=3, rowspan=4)
