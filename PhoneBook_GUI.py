b55fa9e5e8f8bc573d1c5defe3368645ab8a7811
Ilya <hairais@mail.ru>
2020-08-21 15:03:05 +0700

    UPDATE v1.3
    + Создан поиск
    + Добавлено сохранение

--- PhoneBook_GUI.py
+++ PhoneBook_GUI.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk
 import os
 
-version = "v1.2.0001"
+version = "v1.3"
 
 phone_book = dict()
 tel_list = list()
@@ -17,18 +17,42 @@ def clear():
 
 
 def add():
-    tel = input_tel.get()
-    if tel in phone_book:
+    _tel_ = input_tel.get()
+    if _tel_ in phone_book:
         label_info.config(text="! Такой номер уже существует !", fg="red")
     else:
-        value = list()
-        value.append(input_last_name.get())
-        value.append(input_first_name.get())
-        value.append(input_patronymic.get())
-        value.append(input_address.get())
-        phone_book[tel] = value
+        _value_ = list()
+        _value_.append(input_last_name.get())
+        _value_.append(input_first_name.get())
+        _value_.append(input_patronymic.get())
+        _value_.append(input_address.get())
+        phone_book[_tel_] = _value_
 
-        list_tel.insert(END, tel)
+        list_tel.insert(END, _tel_)
+
+
+def save_in_file():
+    with open("PhoneBook.csv", "w") as file:
+        for tel in phone_book:
+            value = phone_book[tel]
+            temp = tel + ";" + value[0] + ";" + value[1] + \
+                   ";" + value[2] + ";" + "\n"
+            file.write(temp)
+
+
+def output_data(tel_):
+    value_ = phone_book[tel_]
+    last_name_ = value_[0]
+    first_name_ = value_[1]
+    patronymic_ = value_[2]
+    address_ = value_[3]
+
+    clear()
+    input_tel.insert(0, tel_)
+    input_last_name.insert(0, last_name_)
+    input_first_name.insert(0, first_name_)
+    input_patronymic.insert(0, patronymic_)
+    input_address.insert(0, address_)
 
 
 def select_list_tel(event):
@@ -36,65 +60,79 @@ def select_list_tel(event):
     i = int(w.curselection()[0])
     tel = w.get(i)
 
-    value = phone_book[tel]
-    last_name = value[0]
-    first_name = value[1]
-    patronymic = value[2]
-    address = value[3]
+    output_data(tel)
 
-    clear()
-    input_tel.insert(0, tel)
-    input_last_name.insert(0, last_name)
-    input_first_name.insert(0, first_name)
-    input_patronymic.insert(0, patronymic)
-    input_address.insert(0, address)
+
+def search():
+    search_text = input_search.get()
+    if search_text in phone_book:
+        output_data(search_text)
+    else:
+        for key, item in phone_book.items():
+            for val in item:
+                if val == search_text:
+                    output_data(key)
+            else:
+                label_info.config(text="Ничего не найдено", fg="red")
 
 
 window = Tk()
 window.title(f"PhoneBook {version}")
-window.geometry("550x240")
+window.geometry("550x270")
+
+label_search = Label(text="Поиск")
+label_search.grid(row=0, column=0, padx=10, pady=5, sticky="e")
+
+input_search = Entry()
+input_search.grid(row=0, column=1, padx=10, pady=5)
 
 label_tel = Label(text="Номер телефона")
-label_tel.grid(row=0, column=0, padx=10, pady=5, sticky="w")
+label_tel.grid(row=1, column=0, padx=10, pady=5, sticky="w")
 
 input_tel = ttk.Entry()
-input_tel.grid(row=0, column=1)
+input_tel.grid(row=1, column=1)
 
 label_last_name = Label(text="Фамилия")
-label_last_name.grid(row=1, column=0, padx=10, pady=5, sticky="w")
+label_last_name.grid(row=2, column=0, padx=10, pady=5, sticky="w")
 
 input_last_name = ttk.Entry()
-input_last_name.grid(row=1, column=1)
+input_last_name.grid(row=2, column=1)
 
 label_first_name = Label(text="Имя")
-label_first_name.grid(row=2, column=0, padx=10, pady=5, sticky="w")
+label_first_name.grid(row=3, column=0, padx=10, pady=5, sticky="w")
 
 input_first_name = ttk.Entry()
-input_first_name.grid(row=2, column=1)
+input_first_name.grid(row=3, column=1)
 
 label_patronymic = Label(text="Отчество")
-label_patronymic.grid(row=3, column=0, padx=10, pady=5, sticky="w")
+label_patronymic.grid(row=4, column=0, padx=10, pady=5, sticky="w")
 
 input_patronymic = ttk.Entry()
-input_patronymic.grid(row=3, column=1)
+input_patronymic.grid(row=4, column=1)
 
 label_address = Label(text="Адрес")
-label_address.grid(row=4, column=0, padx=10, pady=5, sticky="w")
+label_address.grid(row=5, column=0, padx=10, pady=5, sticky="w")
 
 input_address = ttk.Entry()
-input_address.grid(row=4, column=1, pady=10)
+input_address.grid(row=5, column=1, pady=10)
 
 label_info = Label(text="Программа готова к работе...")
-label_info.grid(row=5, column=0, columnspan=4)
+label_info.grid(row=6, column=0, columnspan=26)
 
 button_add = ttk.Button(text="Добавить", command=add)
 button_add.grid(row=1, column=2, padx=38)
+
+button_save = ttk.Button(text="Сохранить", command=save_in_file)
+button_save.grid(row=2, column=2, padx=38)
 
 button_clear = ttk.Button(text="Удалить", command=clear)
 button_clear.grid(row=3, column=2, padx=38)
 
 label_list_tel = Label(text="Список телефонов")
 label_list_tel.grid(row=0, column=3)
+
+button_search = ttk.Button(text="Найти", command=search)
+button_search.grid(row=0, column=2, padx=10)
 
 list_tel = Listbox()
 list_tel.grid(row=1, column=3, rowspan=4)
